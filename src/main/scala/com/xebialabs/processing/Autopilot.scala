20bc7bbfca80703a2cfa9a5eef385209636698c1
ka4eli <zheka238@gmail.com>
2015-06-16 04:08:15 +0300

    autopilot implemented

---
+++ src/main/scala/com/xebialabs/processing/Autopilot.scala
@@ -0,0 +1,38 @@
+package com.xebialabs.processing
+
+import akka.actor.{Props, Actor}
+import com.xebialabs.board.{HexConversions, HexEnemyBoard}
+import com.xebialabs.processing.HexAutopilot.Shoot
+import com.xebialabs.processing.Receptionist.Fire
+
+import scala.util.Random
+
+trait Autopilot[T] {
+  def salvo(n: Int): List[T]
+}
+
+class HexAutopilot(enemyBoard: HexEnemyBoard, gameId: String) extends Actor with Autopilot[String] with HexConversions {
+  val r = Random
+
+  def receive = {
+    case Shoot(n) => context.parent ! Fire(gameId, salvo(n))
+    case _ =>
+  }
+
+  def salvo(n: Int): List[String] = {
+    val unknown = for {
+      i <- 0 to enemyBoard.size._1 - 1
+      j <- 0 to enemyBoard.size._2 - 1
+      if enemyBoard.board(i)(j) == "."
+    } yield shotToHex(i, j)
+    r.shuffle(unknown.toList).take(n)
+  }
+}
+
+object HexAutopilot {
+
+  def props(enemyBoard: HexEnemyBoard, gameId: String) = Props(classOf[HexAutopilot], enemyBoard, gameId)
+
+  case class Shoot(cannons: Int)
+
+}
--- src/main/scala/processing/Autopilot.scala
+++
@@ -1,6 +0,0 @@
-package processing
-
-class Autopilot {
-
-  def shoot = {}
-}
