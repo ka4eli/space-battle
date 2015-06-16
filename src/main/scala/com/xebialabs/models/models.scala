9455aed6f8b5966c41f9bf43cd45566dc7eb22a8
ka4eli <zheka238@gmail.com>
2015-06-16 04:10:53 +0300

    models updated

--- src/main/scala/com/xebialabs/models/models.scala
+++ src/main/scala/com/xebialabs/models/models.scala
@@ -1,4 +1,6 @@
-package models
+package com.xebialabs.models
+
+import com.xebialabs.ship.ShipsGenerator
 
 
 case class NewGameRequest(userId: String, fullName: String, spaceshipProtocol: SpaceshipProtocol, rules: Option[String])
@@ -7,4 +9,6 @@ case class SpaceshipProtocol(hostname: String, port: Int)
 
 case class NewGameResponse(userId: String, fullName: String, gameId: String, starting: String, rules: Option[String])
 
-case class User(userId: String, fullName: String)
+case class User(userId: String, fullName: String, shipsGenerator: Option[ShipsGenerator] = None)
+
+case class ChallengeGame(spaceshipProtocol: SpaceshipProtocol, rules: Option[String])
