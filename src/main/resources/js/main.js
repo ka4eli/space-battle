2e88f9c3d39837ed861f9648eb3c7e6be16c348d
ka4eli <zheka238@gmail.com>
2015-06-16 22:16:21 +0300

    gui partial implementation

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -4,14 +4,30 @@
     var BOARD_COLUMNS = 16;
 
     var SERVER = 'http://localhost:8080/xl-spaceship';
+    var game_id = "game_820";
+    var user_id = "";
+    var isPlaying = false;
 
-    function generateBoard(cl) {
+    function generateBoard(cl, b) {
+        var z = b.map(function (c) {
+            return c.split("");
+        });
+
         var board = [];
 
         for (var i = 0; i < BOARD_ROWS; i++) {
             var rows = [];
             for (var j = 0; j < BOARD_COLUMNS; j++) {
-                rows.push('<div data-y="' + i + '" data-x="' + j + '" class="row-item' + cl + '" id="' + i + 'x' + j + '"></div>');
+                if (z[i][j] == "*") {
+                    rows.push('<div data-x="' + i + '" data-y="' + j + '" class="row-item ship unclickable" id="' + i + 'x' + j + '"></div>');
+                } else if (z[i][j] == "X") {
+                    rows.push('<div data-x="' + i + '" data-y="' + j + '" class="row-item killed unclickable" id="' + i + 'x' + j + '"></div>');
+                } else if (z[i][j] == "-") {
+                    rows.push('<div data-x="' + i + '" data-y="' + j + '" class="row-item missed unclickable" id="' + i + 'x' + j + '"></div>');
+                } else {
+                    rows.push('<div data-x="' + i + '" data-y="' + j + '" class="row-item' + cl + '" id="' + i + 'x' + j + '"></div>');
+
+                }
             }
 
             board.push('<div class="rows">' + rows.join('') + '</div>')
@@ -33,24 +49,34 @@
 
         var fires = {};
 
-        getState('game_3241').then(function (responce) {
-            //responce.self.board
-            console.log(responce.self.board);
-
-            $player.append(generateBoard(""/*responce.self.board*/));
+        getState(game_id).then(function (responce) {
+            $player.append(generateBoard(" unclickable", responce.self.board));
+            $enemy.append(generateBoard(" clickable", responce.opponent.board));
         });
 
         function onFireButtonClicked() {
             console.log(fires);
-            var s = ["1x1","2x2"];
-            $.ajax({
+            var resp = $.ajax({
                 method: 'PUT',
                 dataType: 'json',
                 contentType: "application/json; charset=utf-8",
-                url: SERVER + '/user/game/game_3241/fire',
+                url: SERVER + '/user/game/' + game_id + '/fire',
                 data: JSON.stringify({
                     salvo: _.keys(fires)
                 })
+            });
+            resp.then(function (r) {
+                getState(game_id).then(function (responce) {
+                    if (typeof responce.game.won !== 'undefined') {
+                        var $winner = $('#winner');
+                        $winner.append('<p>Game Over. The winner is ' + responce.game.won + ' </p>')
+                        isPlaying = false;
+                    }
+                    $enemy.empty();
+                    $enemy.append(generateBoard(" clickable", responce.opponent.board));
+                    fires = {};
+                    setTimeout(refreshPlayer, 1000);
+                });
             });
         }
 
@@ -60,18 +86,31 @@
 
         function onEnemyItemClicked(event) {
             var $target = $(event.target);
-            var x = $target.data('x');
-            var y = $target.data('y');
+            var x = intToHex($target.data('x'));
+            var y = intToHex($target.data('y'));
 
             //if ($target.hasClass('row-item')) {
             if ($target.hasClass('clickable')) {
                 $target.toggleClass('active');
-                                           console.log("YESS")
-                fires[x + 'x' + y] = [x, y];
+                if ($target.hasClass('active')) {
+                    fires[x + 'x' + y] = [x, y];
+                } else {
+                    delete fires[x + 'x' + y];
+                }
+                console.log(fires);
             }
         }
 
-        $enemy.append(generateBoard(" clickable"));
+        function refreshPlayer() {
+            getState(game_id).then(function (responce) {
+                $player.empty();
+                $player.append(generateBoard(" clickable", responce.self.board));
+                fires = {};
+            });
+            //setTimeout(refreshPlayer, 500); // you could choose not to continue on failure...
+        }
+
+        //$enemy.append(generateBoard(" clickable"));
 
         $fireButton.on('click', onFireButtonClicked);
 
@@ -79,10 +118,38 @@
         $enemy.on('click', onEnemyItemClicked);
 
         //$enemy.find('#7x0')
+        //setTimeout(refreshPlayer, 500);
+    }
+
+    function waiting() {
+        var $hello = $('#hello');
+        var $available_games = $('#available_games');
+        var $fireButton = $('#fire');
+        $fireButton.hide();
+        $hello.append('<p>Waiting for game</p>')
+        $available_games.append('<a href="#">game_821</a>')
+        $available_games.on('click', onGameJoin);
+
+
+        function onGameJoin(event) {
+            var $target = $(event.target);
+            console.log($target);
+            game_id = $target[0].innerHTML;
+            $hello.empty();
+            $available_games.empty();
+            $fireButton.show();
+            isPlaying = true;
+            init();
+
+        }
     }
 
     $(document).ready(function () {
-        init();
+        if (isPlaying) {
+            init();
+        } else {
+            waiting();
+        }
     });
 
 })(window, document, $);
